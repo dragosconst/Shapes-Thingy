efd1466584b0bb1424596b248c57a76d53fb7b2f
Dragos Constantin <tantarudragos@yahoo.com>
2019-08-06 20:39:57 +0300

    am adaugat si functia de desenat cercuri pt dreptunghi

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -38,7 +38,7 @@ def drawRect(area = 3601):
     tl.forward(latime)
     tl.right(90)
 
-    return [lungime, latime]
+    return [int(lungime), latime]
 
 def drawRhombus(area = 3601):
     angle = rand.randrange(30, 161) # alege un unghi random pentru latura
@@ -68,16 +68,11 @@ def drawSemiCircle(point = [0, 0], radius = 0, angle = 0, side = 1):
     tl.circle(radius, 180)
 
 # urmatoarele functii deseneaza semicercuri peste formele deja desenate, ca sa dea aparenta ca (formele) sunt facute aleatoriu
-def drawCircleOnSquare(area = 3601, specs = [60], stop = 0): # functia asta verifica toate cercurile desenate in patrat pana acum, ca sa determine
-    latura = specs[0]                                        # cum sa deseneze cercul curent; parametrii reprezinta : 
-                                                             # area = aria patratului, are default valoarea minima 3601
-    foundGoodCoord = False                                   # specs = o lista care are diferite date despre forma actuala si semicercurile din ea; o descriere mai detaliata a parametrului specs e mai jos in functie
-    radius = 0                                               # stop = ultimu index din lista specs, default e 0 pt ca specs are minim un element(dimensiunea laturei patratului) 
-    startPoint = 0
-    center = [0, 0]
+# functia asta o fac ca sa fie codul mai lizibil, returneaza o latura pe care nu e desenat niciun semicerc sau -1 daca nu gaseste nicio latura
+def checkSide(specs = [], stop = 0, whichShape = 1): # whichShape functioneaza in felu urmator: valoarea 1 - e patrat, valoarea 2 - e dreptunghi, val 3 - e romb
+    checkIt = []
     whichLatura = 0
-    # aici ma asigur ca se poate desena ceva pe latura actuala
-    checkIt = [] # aici tin minte laturile care au fost deja incercate, ca sa nu le incerce de 2 ori in bucla
+    foundGoodCoord = False
     while foundGoodCoord == False and len(checkIt) < 4: # a doua conditie verifica daca au fost incercate toate laturile, caz in care nu se mai pot
         foundGoodCoord = True                           # adauga cercuri
         whichLatura = rand.randrange(1, 5)
@@ -92,14 +87,26 @@ def drawCircleOnSquare(area = 3601, specs = [60], stop = 0): # functia asta veri
             if thisSideIsChecked:
                 whichLatura = rand.randrange(1, 5)
 
-        ind = 1
+        ind = 1 if whichShape == 1 else (2 if whichShape == 2 else 3)
         while ind <= stop: 
             if specs[ind + 1][1] == whichLatura: # e destul de complicat de desenat mai mult de un semicerc pe latura, nu crek pot rezolva problema
                 foundGoodCoord = False
             ind += 2
         checkIt.append(whichLatura)
 
-    if len(checkIt) == 4:
+    if len(checkIt) == 4: # daca sunt 4 elemente in checkIt, inseamna ca nu exista nicio latura fara vreun semicerc pe ea
+        return -1
+    return whichLatura
+
+def drawCircleOnSquare(area = 3601, specs = [60], stop = 0): # functia asta verifica toate cercurile desenate in patrat pana acum, ca sa determine
+    latura = specs[0]                                        # cum sa deseneze cercul curent; parametrii reprezinta : 
+                                                             # area = aria patratului, are default valoarea minima 3601
+    foundGoodCoord = False                                   # specs = o lista care are diferite date despre forma actuala si semicercurile din ea; o descriere mai detaliata a parametrului specs e mai jos in functie
+    radius = 0                                               # stop = ultimu index din lista specs, default e 0 pt ca specs are minim un element(dimensiunea laturei patratului) 
+    startPoint = 0
+    center = [0, 0]
+    whichLatura = checkSide(specs, stop, 1)
+    if whichLatura == -1:
         return stop
 
     # in while ul de mai jos, whichSide e garantat o latura pe care se poate desena
@@ -204,12 +211,124 @@ def drawCircleOnSquare(area = 3601, specs = [60], stop = 0): # functia asta veri
         specs.append([radius, 2])
     return stop + 4
 
+def drawCircleOnRect(area = 3601, specs = [60, 60], stop = 1): # variabilele au aceasi insemnatate ca la functia precedenta, doar ca specs are acum
+    lungime = specs[0]                                         # 2 elemente minim, latimea si lungime(latimea e pe index 0 si lungimea pe index 1)
+    latime = specs[1]
+
+    minim = int(min(latime, lungime)) # raza unui semicerc nu poate fi, evident, mai mare ca minimul dintre lungime si latime
+    foundGoodCoord = False
+    radius = 0
+    startPoint = 0
+    center =[0, 0]
+    whichLatura = checkSide(specs, stop, 2) # folosesc aceasi codificare pt laturi ca la patrat
+    if whichLatura == -1:
+        return stop
+
+    while foundGoodCoord == False: # while ul asta o sa fie more or less identic cu cel de la patrat
+        foundGoodCoord = True
+        radius = rand.randrange(60, minim)
+        startPoint = rand.randrange(60, minim)
+        if whichLatura == 2 or whichLatura == 4: # daca e pe o latura laterala, coordonatele pt y sunt negative, testoasa incepe la (0, 0)
+            startPoint *= -1 
+        
+        incompatible = True # urmeaza sa verific daca startPoint si radius sunt compatibile cu latura, adica daca cercul "intra" pe latura
+                            # indicata de whichLatura
+        while incompatible:
+            incompatible = False
+            if whichLatura == 1:
+                if startPoint - 2 * radius < 0:
+                    incompatible = True
+            elif whichLatura == 2:
+                if startPoint + 2 * radius > 0:
+                    incompatible = True
+            elif whichLatura == 3:
+                if startPoint + 2 * radius > lungime:
+                    incompatible = True
+            else:
+                if startPoint - 2 * radius < -latime:
+                    incompatible = True
+            
+            if incompatible: # daca valori actuale nu s ok, caut altele    
+                radius = rand.randrange(30, int(minim / 2))
+                if whichLatura == 1 or whichLatura == 2: # valorile minime si maxime pe care le poate lua sunt legate de felul in care turtle
+                    startPoint = rand.randrange(60, int(lungime) if whichLatura == 1 else int(latime))# deseneaza cercuri, se observa experimental cand rulezi programul
+                else:
+                    startPoint = rand.randrange(1, int(lungime) if whichLatura == 3 else int(latime) - 60)
+
+                if whichLatura == 2 or whichLatura == 4: # daca e pe o latura laterala, coordonatele pt y sunt negative                       # coordonatelor pt startPoint
+                    startPoint *= -1
+
+        if whichLatura == 1 : # determina centrul cercului pe care vrea sa l deseneze in functie de latura pe care se afla(testoasa merge mereu 
+           center = [startPoint - radius, 0] #                                                                                 inainte)
+        elif whichLatura == 2:
+           center = [lungime, startPoint - radius]
+        elif whichLatura == 3:
+            center = [startPoint - radius, -latime]
+        else:
+            center = [0, startPoint - radius]
+        
+        ind = 2
+        while ind <= stop and foundGoodCoord == True: # lista specs o sa aiba pe fiecare pozitie para o alta lista care are coordonatele x1, x2(sau y1, y2) ale unui cerc de
+                                                      # pe patrat si pe fiecare pozitie impara are o lista de forma [r, s], unde r e raza cercului si s e latura pe care se afla
+            coords = specs[ind]
+            extras = specs[ind + 1]
+            if abs(radius - extras[0]) <= math.sqrt((coords[0] - center[0]) * (coords[0] - center[0]) + (coords[1] - center[1]) * (coords[1] - center[1])) \
+               and math.sqrt((coords[0] - center[0]) * (coords[0] - center[0]) + (coords[1] - center[1]) * (coords[1] - center[1])) <= radius + extras[0]: # formula asta verifica daca doua cercuri se intersecteaza, scuze ca arata asa nasol
+                foundGoodCoord = False;
+            secondCenter = center # verific daca, de asemenea, si al doilea cerc se intersecteaza cu vreun cerc
+            if whichLatura == 1:  # aici setez efectiv coordonatele
+                secondCenter[1] = -latime
+            elif whichLatura == 2:
+                secondCenter[0] = 0
+            elif whichLatura == 3:
+                secondCenter[1] = 0
+            else:
+                secondCenter[0] = lungime
+                                                                                                                                                                                   # caracterul "\" e folosit ca sa lipeasca doua linii de cod
+            if abs(radius - extras[0]) <= math.sqrt((coords[0] - secondCenter[0]) * (coords[0] - secondCenter[0]) + (coords[1] - secondCenter[1]) * (coords[1] - secondCenter[1])) \
+               and math.sqrt((coords[0] - secondCenter[0]) * (coords[0] - secondCenter[0]) + (coords[1] - secondCenter[1]) * (coords[1] - secondCenter[1])) <= radius + extras[0]: # formula asta verifica daca doua cercuri se intersecteaza, scuze ca arata asa nasol
+                foundGoodCoord = False;
+            ind += 2
+            
+    # aici se incheie while u asta titanic cu conditia cu foundGoodCoord
+
+    # aici desenez semicercurile
+    specs.append(center) # folosesc append pt ca metoa append modifica si obiectul original din "specs"(e totuna cu variabile trimise prin referinta
+    specs.append([radius, whichLatura]) # in C\C++; gen void foo(int& x)
+    if whichLatura == 1:
+        drawSemiCircle([startPoint, 0], radius, 270, 1) # argumentele sunt in ordinea asta: coordonatele unde sa deseneze, raza, unghiul testoasei
+        center[1] = -latime                             # latura
+        drawSemiCircle([startPoint, -latime], radius, 180, 1)  
+        tl.right(270) # rotesc "testoasa" inapoi la rotatia normala
+        specs.append(center)
+        specs.append([radius, 3])
+    elif whichLatura == 2:
+        drawSemiCircle([lungime, startPoint], radius, 0, 2)
+        center[0] = 0
+        drawSemiCircle([0, startPoint], radius, 180, 2)
+        tl.right(180)
+        specs.append(center)
+        specs.append([radius, 4]) 
+    elif whichLatura == 3:
+        drawSemiCircle([startPoint, -latime], radius, 90, 3)
+        center[1] = 0
+        drawSemiCircle([startPoint, 0], radius, 180, 3)
+        tl.right(90) # rotesc "testoasa" inapoi la rotatia normala
+        specs.append(center)
+        specs.append([radius, 1])
+    else:
+        drawSemiCircle([0, startPoint], radius, 180, 4)
+        center[0] = lungime
+        drawSemiCircle([lungime, startPoint], radius, 180, 4)
+        specs.append(center)
+        specs.append([radius, 2])
+    return stop + 4
 
 # functia asta doar apeleaza diverse functii ca sa deseneze o forma random de arie specificata
 def drawRandomShape(area = 3601):
     whichShape = rand.randrange(1, 4) # ahem, din ce am citit in documentatie, randrange nu include si ultimu element(adica alege doar din 1, 2, 3)
     shapeSpecs = []
-    whichShape = 1
+    #whichShape = 2
     if whichShape == 1:
         shapeSpecs = drawSquare(area)
     elif whichShape == 2:
@@ -224,8 +343,9 @@ def drawRandomShape(area = 3601):
             howManySpecs = drawCircleOnSquare(area, shapeSpecs, howManySpecs)
             irregularities -= 1
     elif whichShape == 2:
-        howManySpecs = 0
+        howManySpecs = 1
         while irregularities > 0:
+            howManySpecs = drawCircleOnRect(area, shapeSpecs, howManySpecs)
             irregularities -= 1
 
         
